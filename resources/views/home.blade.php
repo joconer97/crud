fa5dd11e3b1d409ed78a6a27f202deb424cf2e4f
arneljoshua <arneljoshua.payongayon@unc.edu.ph>
2020-02-26 19:57:44 +0800

    Activity

---
+++ resources/views/home.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="{{asset('css/app.css')}}" />
+    
+</head>
+<body>
+
+    <div class="row">
+        <h1 class="title is-1">ToDo Application</h1>
+
+        <form action="api/todo" method="post">
+            <div class="field">
+                <label class="label">ToDo Name</label>
+                <div class="control">
+                <input class="input" type="text" placeholder="ToDo" name="name">
+                </div>
+            </div>
+
+            <div class="field is-grouped">
+                <div class="control">
+                <button class="button is-link">Save</button>
+            </div>
+        </form>
+    </div>
+    
+
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th><abbr title="Position">ToDo Name</abbr></th>
+
+          </tr>
+        </thead>
+        <tbody>
+         @foreach($todos as $todo)
+          <tr>
+            <td>{{$todo->name}}</td>
+            <td><a href="/api/todo/{{$todo->id}}">Delete</a></td>
+          </tr>
+        @endforeach
+        </tbody>
+      </table>
+</body>
+</html>
